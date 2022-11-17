aa3c9b686631b003ff6172544336ea0c06f74815
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-17 19:36:38 +0100

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-  <main class="flex h-screen items-center justify-center bg-gray-100">
+  <main class="flex h-screen items-center justify-center bg-gray-800">
 
     <!-- quiz container -->
     <div class="overflow-hidden bg-white container relative shadow-lg rounded-lg px-12 py-6">
@@ -18,63 +18,35 @@
 
         <!-- timer container -->
         <div class="bg-white shadow-lg p-1 rounded-full w-full h-5 mt-4">
-          <div class="bg-blue-700 rounded-full w-11/12 h-full"></div>
+          <div class="bg-blue-700 rounded-full w-7/12 h-full"></div>
         </div>
 
         <!-- question container -->
         <div class="rounded-lg bg-gray-100 p-2 neumorph-1 text-center font-bold text-gray-800 mt-8">
-          <div class="bg-white p-5">Who is the most powerful avenger in the marvel cinematic universe?</div>
+          <div class="bg-white p-5">{{ currentQuestion.question }}</div>
         </div>
-        
+
         <!-- options container -->
         <div class="mt-8">
-          <!-- option container -->
-          <div class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative">
-            <div
-              class="bg-blue-500 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 -top-0 shadow-md">
-              <p class="transform -rotate-45">+10</p>
-            </div>
-            <div class="rounded-lg font-bold flex p-2">
-              <!-- option id -->
-              <div class="p-3 rounded-lg">
-                A
+          <div v-for="(choice, item) in currentQuestion.choices" :key="item">
+            <!-- option container -->
+            <div class="neumorph-1 option-default bg-gray-100 p-2 rounded-lg mb-3 relative" :ref="optionChosen"
+              @click="onOptionClicked(choice, item)">
+              <div
+                class="bg-blue-500 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 -top-0 shadow-md">
+                <p class="transform -rotate-45">+10</p>
               </div>
-              <div class="flex items-center pl-6">
-                spider-man
-              </div>
-            </div>
-          </div>
-          <!-- option container -->
-          <div class="neumorph-1 option-wrong bg-gray-100 p-2 rounded-lg mb-3 relative">
-            <div
-              class="bg-blue-500 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 -top-0 shadow-md">
-              <p class="transform -rotate-45">+10</p>
-            </div>
-            <div class="rounded-lg font-bold flex p-2">
-              <!-- option id -->
-              <div class="p-3 rounded-lg">
-                B
-              </div>
-              <div class="flex items-center pl-6">
-                hulk
+              <div class="rounded-lg font-bold flex p-2">
+                <!-- option id -->
+                <div class="p-3 rounded-lg">
+                  {{ item }}
+                </div>
+                <div class="flex items-center pl-6">
+                  {{ choice }}
+                </div>
               </div>
             </div>
-          </div>
-          <!-- option container -->
-          <div class="neumorph-1 option-correct bg-gray-100 p-2 rounded-lg mb-3 relative">
-            <div
-              class="bg-blue-500 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold absolute right-0 -top-0 shadow-md">
-              <p class="transform -rotate-45">+10</p>
-            </div>
-            <div class="rounded-lg font-bold flex p-2">
-              <!-- option id -->
-              <div class="p-3 rounded-lg">
-                c
-              </div>
-              <div class="flex items-center pl-6">
-                ironman
-              </div>
-            </div>
+            <!-- end option container -->
           </div>
         </div>
 
@@ -89,6 +61,81 @@
     </div>
   </main>
 </template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+export default {
+  setup() {
+    // data
+    let questionCounter = ref(0)
+    const currentQuestion = ref({
+      question: '',
+      answer: 1,
+      choices: []
+    });
+
+    const questions = [
+      {
+        question: 'Which operator returns true if the two compared values are not equal?',
+        answer: 4,
+        choices: ['<>', '~', '==!', '!==']
+      },
+      {
+        question: 'How is a forEach statement different from a for statement?',
+        answer: 2,
+        choices: [
+          'Only a for statement uses a callback function.',
+          'A for statement is generic, but a forEach statement can be used only with an array.',
+          'Only a forEach statement lets you specify your own iterator.',
+          'A forEach statement is generic, but a for statement can be used only with an array.'
+        ]
+      }
+    ];
+
+    const onQuizStart = () => {
+      currentQuestion.value = questions[questionCounter.value];
+    };
+
+    // lifecycle hooks
+    onMounted(() => {
+      onQuizStart();
+    });
+
+    // methods/functions
+    let itemRef = [];
+    const optionChosen = element => {
+      if (element) {
+        itemRef.push(element);
+      }
+    };
+
+    const onOptionClicked = (choiceSelected, item) => {
+      const divContainer = itemRef[item];
+      const optionId = item + 1;
+      if (currentQuestion.value.answer === optionId) {
+        console.log('you are correct');
+        divContainer.classList.add('option-correct');
+        divContainer.classList.remove('option-default');
+      } else {
+        console.log('you are wrong');
+        divContainer.classList.add('option-wrong');
+        divContainer.classList.remove('option-default');
+      }
+    };
+
+    // return
+    return {
+      optionChosen,
+      currentQuestion,
+      questions,
+      questionCounter,
+      onQuizStart,
+      onOptionClicked
+    }
+  },
+}
+</script>
 
 <style scoped>
 .container {
